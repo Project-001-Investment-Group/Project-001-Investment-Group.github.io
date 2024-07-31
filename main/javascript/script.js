e9af40e50d8433447a1c71c979a8ca8628a40aa4
Kevin Ndou <111686025+kevinndou@users.noreply.github.com>
2024-07-31 09:13:51 +0200

    Delete main directory

--- main/javascript/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const navMenu = document.querySelector('.nav-menu');
-
-    menuToggle.addEventListener('click', () => {
-        navMenu.classList.toggle('active');
-    });
-});
